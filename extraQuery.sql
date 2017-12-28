25b06ddce581d178ea2fed1a154f895f3909c48f
James Hunt <jameshuntwork1997@yahoo.com>
2017-12-28 22:51:37 +0000

    Additional Query Demonstrated
    Query is able to retrieve car information about cars that have had to have parts returned during their repairs due to faults.

---
+++ extraQuery.sql
@@ -0,0 +1,12 @@
+SELECT car.id AS car_id,
+	car.registration,
+	work_plan.id AS work_plan_id,
+	work_plan.work_plan_car_id AS work_plan_car_id,
+	job.id AS job_id, job.job_work_plan_id AS job_work_plan_id,
+	special_part.id AS special_part_id,
+	special_part.part_name,
+	special_part.special_job_id,
+	special_part.return_date
+	FROM car, work_plan, job, special_part
+	WHERE car.id = work_plan.work_plan_car_id AND work_plan.id = job.job_work_plan_id AND job.id = special_part.special_job_id
+	AND special_part.return_date IS NOT NULL;
